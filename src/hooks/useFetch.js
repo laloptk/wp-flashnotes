1690f5268a4542069e13956a3b88e10b4c86e960
Eduardo Sanchez <laloptk@users.noreply.github.com>
2025-08-22 20:34:19 -0600

    feat: Implement ResourceAPIService and update useFetch hook for API interactions

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,22 +1,28 @@
 import {useState, useEffect} from '@wordpress/element';
+import ResourcesAPIService from '../ResourcesAPIService';
 
-const useFetch = () => {
+const useFetch = (type, query_params = {}) => {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState('');
+    
     useEffect(() => {
         const getItems = async () => {
             try {
-            setLoading(true);
-            //const results = await apiFetch($path);
-            
+                setLoading(true);
+                const api = new ResourcesAPIService(type);
+                const results = await api.get(query_params);
+                setData(results);
             } catch(e) {
                 setLoading(false);
+                throw new Error(e);
             } finally {
                 setLoading(false);
             }
         }
 
         getItems();
-    }, []);
+    }, [type]);
 }
+
+export default useFetch;
