56b07b055bc338d3518b45943a2880351f43ea15
Eduardo Sanchez <laloptk@users.noreply.github.com>
2025-12-02 17:28:16 -0600

    Remove debug log

--- includes/Services/DataBaseService.php
+++ includes/Services/DataBaseService.php
@@ -30,7 +30,6 @@ final class DatabaseService implements ServiceInterface {
 	 * Called by register_activation_hook().
 	 */
 	public static function install_schema(): void {
-		error_log('This is triggering');
 		try {
 			$tables = [
 				new CardsTable(),
