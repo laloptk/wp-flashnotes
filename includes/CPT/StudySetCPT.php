97755c8cf6eaf497012af77b4cd81985e13d6c40
Eduardo Sanchez <laloptk@users.noreply.github.com>
2025-12-02 21:44:35 -0600

    Add args to CPT registration

--- includes/CPT/StudySetCPT.php
+++ includes/CPT/StudySetCPT.php
@@ -17,4 +17,21 @@ class StudySetCPT extends BaseCPT {
 	protected function set_plural(): string {
 		return __( 'Study Sets', 'wp-flashnotes' );
 	}
+
+	protected function args(): array {
+		return array(
+			'labels'              => $this->labels(),
+			'menu_icon'           => 'dashicons-index-card',
+			'supports'            => array( 'title', 'editor', 'revisions' ),
+			'show_in_rest'        => true,
+			'public'              => true,
+			'publicly_queryable'  => true,
+			'exclude_from_search' => true,
+			'has_archive'         => false,
+			'rewrite'             => false,
+			'show_ui'             => true,
+			'show_in_menu'        => true,
+			'capability_type'     => 'post',
+		);
+	}
 }
